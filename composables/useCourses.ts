7b13d56bd48b703041ae5dee59454d1905cd36bd
ksm <rkdtjd0909@gmail.com>
2025-08-07 13:24:45 +0900

    useCourses컴포저블 path 프로퍼티 오탈자 수정

--- composables/useCourses.ts
+++ composables/useCourses.ts
@@ -11,7 +11,7 @@ export const useCourses = (): CoursesReturn => {
       rating: item.rating.toFixed(1), // 5 -> 5.0
       reviewsCount: item.reviewsCount.toLocaleString(), // 1000 -> 1,000
       studentCount: item.studentCount.toLocaleString(), // 12345 -> 12,345
-      path: `/course/${item.courseSlug}`,
+      path: `/courses/${item.courseSlug}`,
     })
   );
 
