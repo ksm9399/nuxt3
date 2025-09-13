a69c08f8f87829611a213d04d4d132e4e0e1b6c8
ksm <rkdtjd0909@gmail.com>
2025-09-13 22:06:44 +0900

    캐시 데이터 fetching

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,13 +1,3 @@
 import type { CourseReturn } from '~/types/course';
 
-export const useCourse = async (courseSlug: string): Promise<Maybe<CourseReturn>> => {
-  const {data, error} = await useFetch<CourseReturn>(`/api/courses/${courseSlug}`)
-
-  if (error.value) {
-    throw createError({
-      ...error.value
-    })
-  }
-
-  return data.value
-};
+export const useCourse = (courseSlug: string): Promise<Maybe<CourseReturn>> => useFetchWithCache<CourseReturn>(`/api/courses/${courseSlug}`);
